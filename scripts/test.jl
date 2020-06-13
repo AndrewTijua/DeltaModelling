f765497238a0bc0740be1291ec554e646f325e33
AndrewTijua <ben.10.10@hotmail.co.uk>
2020-06-13 02:32:29 +0100

    something is coming out, results look plausible to my untrained eye

---
+++ scripts/test.jl
@@ -0,0 +1,54 @@
+include("../src/libs/helpers.jl")
+
+using Plots
+plotlyjs()
+
+x = 1:100
+y = 1:100
+
+xs = collect(Float64, x)
+ys = collect(Float64, y)
+
+
+h_mat = convert(Array{Float64, 2}, sqrt.(x) * sqrt.(y)')
+h_mat ./= 16 * maximum(h_mat)
+h_mat .*= -1.
+h_mat .+= 0.02
+
+w_mat = zeros(size(h_mat))
+#w_mat[1,1] = h_mat[1,1] * 1.2
+δ = 0.00035
+w_mat = max.(w_mat, h_mat .- δ)
+for i in x
+    w_mat[i,i] = w_mat[i,i] * 1.2
+end
+plot(xs, ys, w_mat, st = :surface)
+plot!(xs, ys, h_mat, st = :surface)
+
+sbx = sby = 65
+
+d_model = delta_model_abm(h_mat, w_mat, sbx, sby; timestep = 0.5, moore = false)
+
+w_lev(a) = a.w_level
+s_elev(a) = a.s_elev
+w_flow(a) = a.w_flow
+
+adata = [w_lev, s_elev, w_flow]
+
+flow_step!(d_model)
+
+nsteps = 5000
+
+results, _ = run!(d_model, dummystep, flow_step!, nsteps; adata = adata, when = [nsteps])
+k = Array(results.w_lev)
+m = Array(results.s_elev)
+f = Array(results.w_flow)
+wl = reshape(k, size(w_mat))
+se = reshape(m, size(w_mat))
+wf = reshape(f, size(w_mat))
+plot(xs, ys, wl, st = :surface)
+plot!(xs, ys, se, st = :surface)
+
+plot(xs, ys, wl - se .+ δ, st = :surface)
+
+plot(xs, ys, wf, st = :surface)
--- src/libs/helpers.jl
+++ src/libs/helpers.jl
@@ -2,7 +2,7 @@ using StaticArrays
 using LinearAlgebra
 using Agents
 
-mutable struct cell
+mutable struct cell <: AbstractAgent
     #=
     generic cell structure, all nodes of domain contain one (and only one) of these
     id: cell id in abm
@@ -14,17 +14,18 @@ mutable struct cell
         index 2: sediment, sand parcel
         index 3: sediment, mud parcel
     is_sea_boundary: used to check if height should be held at sea level
-    is_wall_boundary: used to check if passable to parcels
     is_inlet_boundary: used to define inlet for water parcels and sediment parcels
     =#
     id::Int64
-    pos::Tuple{Int64, Int64}
+    pos::Tuple{Int64,Int64}
     w_level::Float64
     s_elev::Float64
+    w_flow::Float64
+    in_sed::Float64
+    in_sed_new::Float64
     # visited::Tuple{Bool, Bool, Bool}
     is_sea_boundary::Bool
-    is_wall_boundary::Bool
-    is_inlet_boundary::Bool
+    is_inlet::Bool
 end
 
 # mutable struct water_parcel
@@ -49,3 +50,167 @@ end
 #     pos::Tuple{Int64, Int64}
 #     volume::Float64
 # end
+
+function delta_model_abm(
+    height_matrix,
+    depth_matrix,
+    sbx,
+    sby;
+    timestep = 0.1,
+    eps_smooth = 1e-7,
+    I_0 = 1.7e-4,
+    J_0 = 2.5e-4,
+    c_sigma = 8.5,
+    I_ero = 4e-6,
+    V_ero = 4e-6,
+    theta_ero = -5e-7,
+    c1 = 0.1,
+    c2 = 0.1,
+    moore = false,
+)
+    properties = Dict(
+        :dt => timestep,
+        :ϵ => eps_smooth,
+        :I_0 => I_0,
+        :s_0 => J_0,
+        :c_sigma => c_sigma,
+        :I_ero => I_ero,
+        :V_ero => V_ero,
+        :ero_max => theta_ero,
+        :c1 => c1,
+        :c2 => c2,
+        :moore => moore,
+    )
+
+    grid_dims = size(height_matrix)
+    space = GridSpace(grid_dims, moore = moore)
+
+    model = AgentBasedModel(cell, space; properties = properties)
+
+    _idx = 1
+    for x = 1:grid_dims[1]
+        for y = 1:grid_dims[2]
+            if (x == 1 && y == 1)
+                add_agent_pos!(cell(_idx, (x, y), depth_matrix[x, y], height_matrix[x, y], I_0, J_0, 0.0, false, true), model)
+            elseif ((x < grid_dims[1] && y < grid_dims[2]) || (x == grid_dims[1] && y < sby) || (y == grid_dims[2] && x < sbx))
+                add_agent_pos!(cell(_idx, (x, y), depth_matrix[x, y], height_matrix[x, y], 0.0, 0.0, 0.0, false, false), model)
+            else
+                add_agent_pos!(cell(_idx, (x, y), depth_matrix[x, y], height_matrix[x, y], 0.0, 0.0, 0.0, false, true), model)
+            end
+            _idx += 1
+        end
+    end
+    return model
+end
+
+function h_conduct(Vi, Vj, Hi, Hj, c_sigma)
+    V_avg = 0.5 * (Vi + Vj)
+    H_avg = 0.5 * (Hi + Hj)
+    if (V_avg - H_avg) > 0.0
+        return c_sigma * (V_avg - H_avg)
+    else
+        return 0.0
+    end
+end
+
+function sed_ero_rate(c1, c2, I_ero, V_ero, I_ij, Vi, Vj, ero_max)
+    t1 = c1 * (I_ero - abs(I_ij))
+    t2 = c2 * (V_ero - abs(Vi - Vj))
+    dsij = t1 + t2
+    if dsij > ero_max
+        return dsij
+    else
+        return ero_max
+    end
+end
+
+function J_ij_out_help(J_ik_in, I_out, I_ij_out)
+    if I_ij_out <= 0
+        return 0.0
+    elseif I_out <= 0
+        return 0.0
+    else
+        return (J_ik_in * I_ij_out / I_out)
+    end
+end
+
+function flow_step!(model)
+    for node_c in nodes(model)
+        node = model[get_node_contents(node_c, model)[1]]
+        node.w_flow = 0.0
+    end
+
+    for node_c in nodes(model)
+        node = model[get_node_contents(node_c, model)[1]]
+
+        #set boundary conditions and skip
+        if node.is_sea_boundary
+            node.w_level = 0.0
+            node.in_sed = 0.0
+            node.in_sed_new = 0.0
+        end
+
+        #check if inlet and if so add flow and sediment
+        if node.is_inlet
+            node.w_flow = model.I_0
+            node.in_sed = model.s_0
+            node.in_sed_new = model.s_0
+        end
+
+        #calculate sum(abs(I_ik^out))
+        out_water = 0.0
+        for neighbor_c in node_neighbors(node_c, model)
+            neighbor = model[get_node_contents(neighbor_c, model)[1]]
+            I_ij = (node.w_level - neighbor.w_level) * h_conduct(node.w_level, neighbor.w_level, node.s_elev, neighbor.s_elev, model.c_sigma)
+            if I_ij > 0
+                out_water += I_ij
+            end
+        end
+
+        #calculate erosive flow and distribute
+        for neighbor_c in node_neighbors(node_c, model)
+            neighbor = model[get_node_contents(neighbor_c, model)[1]]
+
+            sigma_ij = h_conduct(node.w_level, neighbor.w_level, node.s_elev, neighbor.s_elev, model.c_sigma)
+            I_ij = sigma_ij * (node.w_level - neighbor.w_level)
+
+            node.w_flow = node.w_flow + I_ij
+            #neighbor.w_flow = neighbor.w_flow - I_ij
+
+            dS_ij = sed_ero_rate(model.c1, model.c2, model.I_ero, model.V_ero, I_ij, node.w_level, neighbor.w_level, model.ero_max)
+            nH_i = node.s_elev + 0.5 * model.dt * dS_ij
+            nH_j = neighbor.s_elev + 0.5 * model.dt * dS_ij
+
+            J_ij_out = J_ij_out_help(node.in_sed, out_water, I_ij)
+            node.in_sed -= J_ij_out
+            neighbor.in_sed_new += J_ij_out
+
+            node.s_elev = nH_i
+            neighbor.s_elev = nH_j
+        end
+    end
+
+    for node_c in nodes(model)
+        node = model[get_node_contents(node_c, model)[1]]
+        #change water level for non inlet nodes
+        node.in_sed = node.in_sed_new
+        node.in_sed_new = 0.0
+        if !((node.is_inlet) || (node.is_sea_boundary))
+            node.w_level = (node.w_level - node.w_flow * model.dt)
+        end
+    end
+
+    for node_c in nodes(model)
+        node = model[get_node_contents(node_c, model)[1]]
+        if node.w_level > node.s_elev
+            h_neigh = 0.0
+            num_neigh = 0
+            for neighbor_c in node_neighbors(node_c, model)
+                neighbor = model[get_node_contents(neighbor_c, model)[1]]
+                num_neigh += 1
+                h_neigh += neighbor.s_elev
+            end
+            node.s_elev = (1 - model.ϵ) * node.s_elev + model.ϵ / num_neigh * h_neigh
+        end
+    end
+end
