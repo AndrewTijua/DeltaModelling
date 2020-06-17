589396d453f98dc5c81584aeb3863b83e121d992
AndrewTijua <ben.10.10@hotmail.co.uk>
2020-06-17 01:18:34 +0100

    jesus how is this model specified

--- scripts/test.jl
+++ scripts/test.jl
@@ -8,16 +8,19 @@ plotlyjs()
 
 Random.seed!(1234)
 
-x = 1:100
-y = 1:100
+xm = 75
+ym = 75
+
+x = 1:xm
+y = 1:ym
 
 xs = collect(Float64, x)
 ys = collect(Float64, y)
 
 
-h_mat = zeros(100, 100)
+h_mat = zeros(xm, ym)
 for col = y, row = x
-    h_mat[row, col] = 100. - 0.85*row - 0.85*col + 2.6 * rand()
+    h_mat[row, col] = 100. - 2.85*row - 2.85*col + 2.6 * rand()
 end
 h_mat ./= 1000.
 
@@ -30,24 +33,25 @@ h_mat ./= 1000.
 
 w_mat = zeros(size(h_mat))
 #w_mat[1,1] = h_mat[1,1] * 1.2
-δ = 0.0015
-w_mat = max.(w_mat, h_mat .- δ)# + 0.0005 * rand(100, 100)
-#h_mat -= 0.01 * BandedMatrix(Ones(100, 100), (15,15))
-# for i in x
-#     w_mat[i,i] = w_mat[i,i] * 1.2
+δ = 0.0025
+# + 0.0005 * rand(100, 100)
+
+
+w_mat = max.(w_mat, h_mat .- δ)
+# nblock = 5
+# for col = 1:nblock+4, row = 1:nblock+4
+#     w_mat[row,col] = 1.04 * h_mat[row,col]
 # end
 
-nblock = 5
-for col = 10:nblock+9, row = 10:nblock+9
-    w_mat[row,col] = 1.02 * h_mat[row,col]
-end
+w_mat[1,1] = h_mat[1,1] * 1.04
 
+#h_mat -= 0.007 * BandedMatrix(Ones(xm, ym), (1,1))
 plot(xs, ys, w_mat, st = :surface, c = :blues)
 plot!(xs, ys, h_mat, st = :surface, c = :greens)
 
 sbx = sby = 10
 
-d_model = delta_model_abm_f(h_mat, w_mat, sbx, sby; timestep = 5., moore = true, c_sigma = 8.5, c1 = 0.0, c2 = 0.0)
+d_model = delta_model_abm_f(h_mat, w_mat, sbx, sby; timestep = 5., moore = true, c_sigma = 8.5, c1 = 0.5, c2 = 0.0)
 
 w_lev(a) = a.w_lev
 s_ele(a) = a.s_ele
@@ -58,7 +62,7 @@ adata = [w_lev, s_ele, w_flo, i_sed]
 
 flow_new_delta!(d_model)
 
-nsteps = 400
+nsteps = 500
 
 results, _ = run!(d_model, dummystep, flow_new_delta!, nsteps; adata = adata, when = [nsteps])
 k = Array(results.w_lev)
@@ -71,12 +75,12 @@ wf = reshape(f, size(w_mat))
 si = reshape(s, size(w_mat))
 plot(xs, ys, wl, st = :surface, c = :blues)
 plot!(xs, ys, se, st = :surface, c = :greens)
-#plot(xs, ys, wf, st = :surface, c = :reds)
 
-# plot(xs, ys, si, st = :surface, c = :cividis)
+# plot(xs, ys, wf, st = :surface, c = :reds)
+# plot!(xs, ys, si*2000., st = :surface, c = :cividis)
 #
 # plot(xs, ys, wl - w_mat, st = :surface, c = :blues)
-# plot!(xs, ys, se - h_mat, st = :surface, c = :greens)
+# plot(xs, ys, se - h_mat, st = :surface, c = :greens)
 
 # plot(xs, ys, wl - se .+ δ, st = :surface)
 
@@ -84,5 +88,5 @@ plot!(xs, ys, se, st = :surface, c = :greens)
 
 # using ProfileView
 # @profview run!(d_model, dummystep, flow_step!, nsteps; adata = adata, when = [nsteps])
-# @profview run!(d_model, dummystep, flow_step!, 100; adata = adata, when = [100])
+# @profview run!(d_model, dummystep, flow_new_delta!, 10; adata = adata, when = [10])
 # @time flow_step!(d_model)
--- src/libs/structs.jl
+++ src/libs/structs.jl
@@ -38,10 +38,11 @@ function delta_model_abm_f(
     c_sigma = 8.5,
     I_ero = 4e-6,
     V_ero = 4e-6,
-    theta_ero = -5e-7,
+    theta_ero = -5e-5,
     c1 = 0.1,
     c2 = 0.1,
     moore = false,
+    saturate = 0.05,
 )
     properties = Dict(
         :dt => timestep,
@@ -56,6 +57,7 @@ function delta_model_abm_f(
         :c1 => c1,
         :c2 => c2,
         :moore => moore,
+        :saturate => saturate,
     )
 
     grid_dims = size(height_matrix)
@@ -66,7 +68,7 @@ function delta_model_abm_f(
     _idx = 1
     for x = 1:grid_dims[1]
         for y = 1:grid_dims[2]
-            if (x == 10 && y == 10)
+            if (x == 1 && y == 1)
                 add_agent_pos!(
                     delta_agent(
                         _idx,
@@ -75,8 +77,8 @@ function delta_model_abm_f(
                         depth_matrix[x, y],
                         height_matrix[x, y],
                         height_matrix[x, y],
-                        I_0,
-                        I_0,
+                        -I_0,
+                        -I_0,
                         J_0,
                         J_0,
                         false,
@@ -93,10 +95,10 @@ function delta_model_abm_f(
                         depth_matrix[x, y],
                         height_matrix[x, y],
                         height_matrix[x, y],
-                        I_0,
-                        I_0,
-                        J_0,
-                        J_0,
+                        0.0,
+                        0.0,
+                        0.0,
+                        0.0,
                         false,
                         false,
                     ),
@@ -111,10 +113,10 @@ function delta_model_abm_f(
                         depth_matrix[x, y],
                         height_matrix[x, y],
                         height_matrix[x, y],
-                        I_0,
-                        I_0,
-                        J_0,
-                        J_0,
+                        0.0,
+                        0.0,
+                        0.0,
+                        0.0,
                         true,
                         false,
                     ),
@@ -138,7 +140,11 @@ function hydraulic_conductivity(node::delta_agent, neighbour::delta_agent, c_sig
 end
 
 function pairwise_flux(node::delta_agent, neighbour::delta_agent, c_sigma::Float64)
-    return hydraulic_conductivity(node::delta_agent, neighbour::delta_agent, c_sigma::Float64) * (node.w_lev - neighbour.w_lev)
+    hc = hydraulic_conductivity(node::delta_agent, neighbour::delta_agent, c_sigma::Float64)
+    dh = (node.w_lev - neighbour.w_lev)
+    @assert !isnan(hc)
+    @assert !isnan(dh)
+    return hc * dh
 end
 
 function sum_flux(node::delta_agent, model)
@@ -167,60 +173,110 @@ end
 
 
 function landscape_modify!(node::delta_agent, neighbour::delta_agent, model)
-    s_e_r::Float64 = sedimentation_erosion_rate(node, neighbour, model)
+    # if node.w_lev > node.s_ele
+    s_e_r_raw::Float64 = sedimentation_erosion_rate(node, neighbour, model)
+    @assert !isnan(s_e_r_raw)
+    s_e_r::Float64 = min(node.i_sed, s_e_r_raw)
     timestep = model.dt
-    node.s_ele_p += 0.5 * timestep * s_e_r
-    neighbour.s_ele_p += 0.5 * timestep * s_e_r
+    #node.i_sed_p -= s_e_r
+    #neighbour.i_sed_p += s_e_r
+    setfield!(node, :i_sed_p, node.i_sed_p - s_e_r)
+    #setfield!(neighbour, :i_sed_p, neighbour.i_sed_p + s_e_r)
+    # node.s_ele_p += 0.5 * timestep * s_e_r
+    # neighbour.s_ele_p += 0.5 * timestep * s_e_r
+    setfield!(node, :s_ele_p, node.s_ele_p + 0.5 * timestep * s_e_r)
+    setfield!(neighbour, :s_ele_p, neighbour.s_ele_p + 0.5 * timestep * s_e_r)
+    # end
 end
 
 function water_depth_modify!(node::delta_agent, model)
     s_flux = sum_flux(node, model)
     timestep = model.dt
-    node.w_lev_p = node.w_lev - timestep * s_flux
+    @assert !isnan(s_flux)
+    node.w_flo = node.w_flo + s_flux
+    #node.w_lev_p = node.w_lev - timestep * s_flux
+    @assert !isnan(node.w_flo)
+    setfield!(node, :w_lev_p, node.w_lev - timestep * node.w_flo)
 end
 
 function sediment_pairwise(node::delta_agent, neighbour::delta_agent, model)
     s_flux_out = sum_flux_out(node, model)
-    pw_flux = pairwise_flux(node, neighbour, model)
-    inflowing_sediment = node.i_sed
-    outflowing_flux_pairwise = pw_flux * inflowing_sediment / s_flux_out
+    pw_flux = pairwise_flux(node, neighbour, model.c_sigma)
+    inflowing_sediment = node.i_sed_p
+    @assert !isnan(s_flux_out)
+    @assert !isnan(pw_flux)
+    @assert !isnan(inflowing_sediment)
+    if s_flux_out > 0
+        outflowing_flux_pairwise = max(0.0, pw_flux) * max(inflowing_sediment, 0.0) / s_flux_out
+    else
+        outflowing_flux_pairwise = 0.0
+    end
+    @assert !isnan(outflowing_flux_pairwise) "$s_flux_out, $pw_flux, $inflowing_sediment"
     return outflowing_flux_pairwise
 end
 
-function distribute_sediment(node::delta_agent, model) end
-
-function smooth_node!(node::delta_agent, model)
-    if node.s_ele < node.w_lev
-        term_1::Float64 = (1.0 - model.ϵ) * node.s_ele
-        h_neigh::Float64 = 0.0
-        n_neigh::UInt8 = 0
-        w_neigh::Float64 = 0.0
-        for neighbour_c in node_neighbors(node, model)
-            neighbour = model[get_node_contents(neighbour_c, model)[1]]
-            h_neigh += neighbour.s_ele
-            w_neigh += neighbour.w_lev
-            n_neigh += 1
-        end
-        term_2::Float64 = 1.0 / (n_neigh) * model.ϵ * h_neigh
-        node.s_ele_p = term_1 + term_2
-        # term_3::Float64 = (1.0 - 2.0 * model.ϵ) * node.w_lev_p
-        # term_4::Float64 = 2.0 / (n_neigh) * model.ϵ * w_neigh
-        # node.w_lev_p = term_3 + term_4
+function distribute_sediment(node::delta_agent, model)
+    for neighbour_c in node_neighbors(node, model)
+        neighbour = model[get_node_contents(neighbour_c, model)[1]]
+        of_sed_n::Float64 = sediment_pairwise(node, neighbour, model)
+        @assert !isnan(of_sed_n)
+        setfield!(node, :i_sed_p, node.i_sed_p - of_sed_n)
+        setfield!(neighbour, :i_sed_p, neighbour.i_sed_p + of_sed_n)
     end
 end
 
-function post_pro!(node::delta_agent)
-    if (node.s_ele > 0.0) && (!node.is_inlet) && (!node.is_sea_boundary)
-        node.w_lev = node.w_lev_p
+function smooth_node!(node::delta_agent, model)
+    if (node.s_ele > 0.0) && (!node.is_sea_boundary) && (!node.is_inlet) && (node.w_lev > node.s_ele)
+        h_neigh::Float64 = 0.0
+        n_neigh::UInt8 = 0
+        w_neigh::Float64 = 0.0
+        s_neigh::Float64 = 0.0
+        for neighbour_c in node_neighbors(node, model)
+            @inbounds neighbour = model[get_node_contents(neighbour_c, model)[1]]
+            if neighbour.s_ele < neighbour.w_lev
+                h_neigh += neighbour.s_ele
+                w_neigh += neighbour.w_lev
+                s_neigh += neighbour.i_sed
+                n_neigh += 1
+            end
+        end
+        if n_neigh > 1
+            term_1::Float64 = (1.0 - model.ϵ) * node.s_ele
+            term_2::Float64 = (1.0 / (n_neigh)) * model.ϵ * h_neigh
+            node.s_ele = term_1 + term_2
+
+            term_3::Float64 = (1.0 -  50.0 * model.ϵ) * node.w_lev
+            term_4::Float64 = 50.0 / (n_neigh) * model.ϵ * w_neigh
+            node.w_lev = term_3 + term_4
+
+            term_5::Float64 = (1.0 - 10.0 * model.ϵ) * node.i_sed_p
+            term_6::Float64 = 10.0 / (n_neigh) * model.ϵ * s_neigh
+            node.i_sed_p = term_5 + term_6
+        end
+    end
+end
+
+function post_pro!(node::delta_agent, model)
+    if (node.s_ele > 0.0) && (!node.is_sea_boundary) && (!node.is_inlet)
+        node.w_lev = max(node.w_lev_p, 0.0)
     end
     node.s_ele = node.s_ele_p
     node.w_flo = node.w_flo_p
-    node.i_sed = node.i_sed_p
+    node.i_sed = clamp(node.i_sed_p, 0.0, model.saturate)
+    # node.i_sed = node.i_sed_p
+    node.w_lev_p = 0.0
+    # node.s_ele_p = 0.0
+    node.w_flo_p = 0.0
+    node.i_sed_p = 0.0
+    @assert !isnan(node.s_ele)
+    @assert !isnan(node.w_lev)
+    @assert !isnan(node.i_sed)
+    @assert !isnan(node.w_flo)
 end
 
 function flow_new_delta!(model)
     for node_c in nodes(model)
-        node = model[get_node_contents(node_c, model)[1]]
+        @inbounds node = model[get_node_contents(node_c, model)[1]]
 
         #set boundary conditions and skip
         if node.is_sea_boundary
@@ -236,7 +292,6 @@ function flow_new_delta!(model)
             node.w_flo = -model.I_0
             node.i_sed = model.s_0
             node.i_sed_p = model.s_0
-            continue
         end
 
         #steps 1-3
@@ -244,14 +299,19 @@ function flow_new_delta!(model)
 
         #steps 4-7
         for neighbour_c in node_neighbors(node, model)
-            neighbour = model[get_node_contents(neighbour_c, model)[1]]
-            landscape_modify!(node, neighbour, model)
+            if node.s_ele > node.w_lev
+                @inbounds neighbour = model[get_node_contents(neighbour_c, model)[1]]
+                landscape_modify!(node, neighbour, model)
+            end
         end
+
         #step 8
-        smooth_node!(node, model)
+
     end
     for node_c in nodes(model)
-        node = model[get_node_contents(node_c, model)[1]]
-        post_pro!(node)
+        @inbounds node = model[get_node_contents(node_c, model)[1]]
+        distribute_sediment(node, model)
+        post_pro!(node, model)
+        smooth_node!(node, model)
     end
 end
